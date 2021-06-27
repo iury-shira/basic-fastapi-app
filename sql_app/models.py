fe096fe650eab83f8f3f5e6dc4b9f6267f2efea5
iury-shira <iuryresende628@gmail.com>
2021-06-27 16:59:43 -0300

    Basic CRUD with users relationship

--- sql_app/models.py
+++ sql_app/models.py
@@ -1,12 +1,26 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
 from .database import Base
 
 
 class Message(Base):
-    __tablename__ = "blogs"
+    __tablename__ = "messages"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    user = relationship("User", back_populates="messages")
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
+
+    messages = relationship("Message", back_populates="user")
--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,6 +1,32 @@
 from pydantic import BaseModel
+from typing import List
 
 
 class Message(BaseModel):
     title: str
     body: str
+
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+
+    class Config:
+        orm_mode = True
+
+
+class ShowUserWithMessages(ShowUser):
+    message: List[Message] = []
+
+
+class ShowMessage(Message):
+    user: ShowUser
+
+    class Config:
+        orm_mode = True
